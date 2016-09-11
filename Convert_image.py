39a760b2dea0c6ddf866a7b7225cf22d255b6b78
Grottersha123 <grottersha123@gmail.com>
2016-09-12 02:25:52 +0300

    Convert images

---
+++ Convert_image.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import os, sys
+from PIL import Image
+import subprocess
+
+def convert(path,form):
+   a = os.listdir(path)
+   for i in a:
+        if i.endswith('.jpg') or i.endswith('.png'):
+            out = i.replace('.jpg',form)
+            #out = i.replace('.png',form)
+        Image.open(path+i).save(path+out)
+        os.remove(path+i)
+   #subprocess.check_output(path,)
